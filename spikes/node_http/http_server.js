582ae448b228ae7e54f6387951fe9f53a635ed5b
Mark Lybrand <mlybrand@gmail.com>
2014-09-24 21:56:54 -0700

    Made HTTP spike pass lint

---
+++ spikes/node_http/http_server.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var http = require("http");
+
+var server = http.createServer();
+
+server.on("request", function(request, response) {
+    console.log("Received request");
+    var body = "<html><head><title>Node HTTP Spike</title></head>" +
+            "<body><p>This is a spkie of Node's HTTP server.</p></body></html>";
+    response.end(body);
+});
+
+server.listen(8080);
+
+console.log("Server started");
