392148650ccc50e913ae83dfa2cf5558ed8f6252
Mark Lybrand <mlybrand@gmail.com>
2014-09-26 22:29:35 -0700

    Got our first client/server test working

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -3,11 +3,19 @@
 var server = require("./server.js");
 var http = require("http");
 
-exports.testHttpServer = function(test) {
-    server.start();
-
-    http.get("http://localhost:8080", function(response) {
-
+exports.tearDown = function(done) {
+    server.stop(function() {
+        done();
     });
-    test.done();
+};
+
+//TODO: handle case where stop() is called before start()
+//TODO: test-drive stop() callback
+
+exports.testServerRespondsToGetRequests = function(test) {
+    server.start();
+    http.get("http://localhost:8080", function(response) {
+        response.on("data", function() {});
+        test.done();
+    });
 };
