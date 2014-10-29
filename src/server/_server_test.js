8123bf6a5c9ee1537f7add699483dd9f3f645aed
Mark Lybrand <mlybrand@gmail.com>
2014-10-28 22:22:46 -0700

    moving dev box

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -28,9 +28,15 @@ exports.test_serverReturnsHelloWorld = function(test) {
 exports.test_serverServesAFile = function(test) {
     var testDir = "generated/test";
     var testFile = testDir + "/test.html";
-    fs.writeFileSync(testFile, "Hello world");
+    try {
+        fs.writeFileSync(testFile, "Hello world");
+        test.done();
+    }
+    finally {
+        fs.unlinkSync(testFile);
+        test.ok(!fs.existsSync(testFile), "file should have been deleted");
 
-    test.done();
+    }
 };
 
 exports.test_serverRequiresPortNumber = function(test) {
