413f775666c0ab7f6c29dfbff3bebbe1e912e9cb
Mark Lybrand <mlybrand@gmail.com>
2014-11-18 22:59:10 -0800

    added package.json for heroku

--- src/_smoke_test.js
+++ src/_smoke_test.js
@@ -7,6 +7,8 @@
 
     var child_process = require("child_process");
     var http = require("http");
+    var fs = require("fs");
+    var procfile = require("procfile");
     var child;
 
     exports.setUp = function(done) {
@@ -21,15 +23,16 @@
     };
 
     exports.test_canGetHomePage = function (test) {
-        httpGet("http://localhost:8080", function (response, receivedData) {
+        httpGet("http://localhost:5000", function (response, receivedData) {
             var foundHomePage = receivedData.indexOf("WeeWikiPaint home page") !== -1;
             test.ok(foundHomePage, "home page should have contained test marker");
             test.done();
         });
     };
 
+    // TODO: Factor out common server name
     exports.test_canGet404Page = function(test) {
-        httpGet("http://localhost:8080/nonexistent.html", function (response, receivedData) {
+        httpGet("http://localhost:5000/nonexistent.html", function (response, receivedData) {
             var foundHomePage = receivedData.indexOf("WeeWikiPaint 404 page") !== -1;
             test.ok(foundHomePage, "404 page should have contained test marker");
             test.done();
@@ -37,11 +40,22 @@
     };
 
     function runServer(callback) {
-        child = child_process.spawn("node", ["src/server/weewikipaint", "8080"]);
+        var commandLine = parseProcFile();
+        child = child_process.spawn(commandLine.command, commandLine.options);
         child.stdout.setEncoding("utf8");
         child.stdout.on("data", function(chunk) {
-            if (chunk.trim() === "Server started") callback();
+            if (chunk.trim().indexOf("Server started") !== -1) callback();
         });
+    }
+
+    function parseProcFile() {
+        var fileData = fs.readFileSync("Procfile", "utf8");
+        var webCommand = procfile.parse(fileData).web;
+        webCommand.options = webCommand.options.map(function(element) {
+            if (element === "$PORT") return "5000";
+            else return element;
+        });
+        return webCommand;
     }
 
     function httpGet(url, callback) {
