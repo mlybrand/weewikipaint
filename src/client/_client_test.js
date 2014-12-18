762ee30d97c7f1bab3cafc812012d512c8181dcb
Mark Lybrand <mlybrand@gmail.com>
2014-12-18 00:45:57 -0800

    Migrated to expect.js -- tests now work on all targeted browsers

---
+++ src/client/_client_test.js
@@ -0,0 +1,11 @@
+//var assert = chai.assert;
+
+describe("Nothing", function() {
+
+    it("should run", function() {
+        //assert.equal("foo", "foo");
+        //"foo".should.equal("foo");
+        expect("foo").to.equal("foo");
+    });
+
+});
